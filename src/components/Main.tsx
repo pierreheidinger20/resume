cbe83a33aa05e5d266f8fcef39426ecf7184534a
PIERRE HEIDINGER <150283183+pboronda@users.noreply.github.com>
2025-04-07 12:04:43 -0300

    first commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Main.scss';
+
+function Main() {
+
+  return (
+    <div className="container">
+      <div className="about-section">
+        <div className="image-wrapper">
+          <img src="https://media.licdn.com/dms/image/v2/C4E03AQGxpo9ndep5bg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1543576655829?e=1749686400&v=beta&t=pKHNdWv0Xp62FwSS1eawT6FzK5vmpfWsTnA8SEgPtAE" alt="Avatar" />
+        </div>
+        <div className="content">
+          <div className="social_icons">
+            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+          <h1>Pierre Heidinger</h1>
+          <p>Software Engineer</p>
+
+          <div className="mobile_social_icons">
+            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
